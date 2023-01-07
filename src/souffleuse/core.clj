f8afcde6f9c5cd05f68fe176bdf43f60b77fc133
Timo Kramer <fw-git@timokramer.de>
2023-01-07 10:53:15 +0000

    feat: add rocketchat announcement

--- src/souffleuse/core.clj
+++ src/souffleuse/core.clj
@@ -12,8 +12,8 @@
             [aero.core :as aero]
             [clojure.string :as s]
             [clojure.java.io :as io])
-  (:import (javax.crypto Mac)
-           (javax.crypto.spec SecretKeySpec)))
+  (:import [javax.crypto Mac]
+           [javax.crypto.spec SecretKeySpec]))
 
 (def config (aero/read-config (clojure.java.io/resource "config.edn")))
 
@@ -33,7 +33,7 @@
 (def rocketchat-channel (get-in config [:rocketchat :channel]))
 (def rocketchat-secret (get-in config [:rocketchat :secret]))
 (def rocketchat-token (get-in config [:rocketchat :token]))
-(def rocketchat-hook-url (str "https://chat.lambdaforge.io/hooks/"rocketchat-token"/"rocketchat-secret))
+(def rocketchat-hook-url (str "https://chat.lambdaforge.io/hooks/" rocketchat-token "/" rocketchat-secret))
 
 
 (defn log-request [d]
@@ -97,18 +97,20 @@
                          :params {:status message})
       (log/error "Twitter secrets not provided")))
   body)
+
 (defn trigger-rocketchat-announcement [body]
-		(let [[release repository] ((juxt :release :repository) body)
-								message (format "Version %s of %s was just released. Take a look at the changelog over on GitHub: %s"
-                        (:tag_name release)
-                        (:name repository)
-                        (:html_url release))
-								json (json/generate-string {:username "rocket.chat"
-                                    :channel rocketchat-channel
-                                    :text message})]
+  (if rocketchat-hook-url
+    (let [[release repository] ((juxt :release :repository) body)
+          message (format "Version %s of %s was just released. Take a look at the changelog over on GitHub: %s"
+                    (:tag_name release)
+                    (:name repository)
+                    (:html_url release))
+          json (json/generate-string {:username "rocket.chat"
+                                      :channel rocketchat-channel
+                                      :text message})]
       (clnt/post rocketchat-hook-url {:headers {"content-type" "application/json"}
                                       :body json}))
-    (log/error "RocketChat Hook URL not provided")
+    (log/error "RocketChat Hook URL not provided"))
   body)
 
 (defn check-if-release [body]
@@ -172,6 +174,9 @@
   (def payload (json/parse-string (slurp "test/payload.sample.json") true))
   ((juxt :release :repository) payload)
 
-  (-main)
+  (trigger-rocketchat-announcement {:release {:tag_name "0.0.0"
+                                              :html_url "foobar.com"}
+                                    :repository {:name "foobar"}})
 
+  (-main)
   @(clnt/post "http://localhost:3000/github/release" {:headers {"Content-Type" "application/json"} :body (slurp "test/payload.sample.json")}))
